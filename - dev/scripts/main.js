f365d80a4f923a0af387ef2664217069b30cc4b6
Igor Ivanov <igorpi87@yandex.ru>
2016-02-28 23:36:39 +0300

    home work 2

---
+++ - dev/scripts/main.js
@@ -0,0 +1,102 @@
+var sliderWidget = (function(){
+
+	var _insertValues = function (){
+		var from = $this.closest('.filter__slider').find('.filter__slider-input_from'),
+			to = $this.closest('.filter__slider').find('.filter__slider-input_to');
+		var values = $this.slider('option', 'values');	
+
+		from.val(values[0]);
+		to.val(values[1]);
+	}
+
+	return {
+		init: function(){
+
+			$('.filter__slider-element').each(function(){
+				var $this = $(this),
+					min = parseInt($this.data('min')),
+					max = parseInt($this.data('max'));
+
+				$this.slider({
+				    range: true,
+				    min: min,
+				    max: max,
+				    values: [ min, max ],
+				    slide: function() {
+				     	//_insertValues($this);
+			    	},
+			    	create: function() {
+			    		//_insertValues($this);
+			    	}
+		  	    });
+			    // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+			    //   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+					// }
+			});
+
+		}		
+	}
+}());
+
+
+// var ViewChange = (function(){
+
+//     var _previousClass = '';
+
+// 	var _changeState = function($this) {
+// 		var
+// 			item = $this.closest('.sort__view-item'),
+// 			view = item.data('view'),
+// 			listOfItems = $('#products__list'),
+// 			modPrefix = 'products__list_',
+// 			classOfViewState = modPrefix + view;
+
+// 		if (_previousClass == '') {
+// 			_previousClass = listOfItems.attr('class');
+// 		}
+// 		listOfItems.attr('class', _previousClass + ' ' + classOfViewState);
+
+// 	};
+
+
+// 	return {
+// 		init: function() {
+// 			$('.sort__view-link').on('click', function(e) {
+// 				e.preventDefault();
+// 				_changeState($(this));
+// 				console.log('sss');
+// 			})
+// 		}
+// 	}
+// }());
+
+// var RatingWidget = (function (){
+
+// 	var 
+
+// 	return{
+// 		init: function() {
+// 			$('.products__rating').each(function(){
+// 				console.log($(this).data('raiting'));
+// 			});
+// 		}
+// 	}
+// })();
+
+$(document).ready(function(){
+
+	// if ($('.products__rating').length) {
+	// 	RatingWidget.init();
+	// }
+
+
+	if($('.filter__slider-element').length) {
+		sliderWidget.init();
+	}
+
+	if ($('.sort__select-elem').length) {
+		$('.sort__select-elem').select2({
+				minimumResaultsForSearch: Infinity
+		});
+	}
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+To install my project, you have to:
+
+1. gem install compass
+2. gem install sass-globbing
+3. npm install
+4. gulp
+
+Ruby required!
